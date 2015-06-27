5b185c012d279589ef26d2e328d4fcca066cbee9
Thomas Prislac <pdxvoiceteacher@gmail.com>
2015-06-27 01:24:02 -0700

    initial upload

---
+++ sound-mouseover.js
@@ -0,0 +1,21 @@
+<!--
+
+function playclip() {
+if (navigator.appName == "Microsoft Internet Explorer") {
+if (document.all)
+ {
+  document.all.sound.src = "vii.mp3";
+ }
+}
+
+else {
+{
+var audio = document.getElementsByTagName("audio")[0];
+audio.play();
+}
+}
+}
+
+//-->
+
+
